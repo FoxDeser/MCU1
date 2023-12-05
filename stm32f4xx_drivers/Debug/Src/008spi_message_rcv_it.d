390c47cc4aa9eef48ed2431792d7757e60757302
Your Name <you@example.com>
2023-12-05 20:52:19 +0700

    Update the prj

---
+++ stm32f4xx_drivers/Debug/Src/008spi_message_rcv_it.d
@@ -0,0 +1,13 @@
+Src/008spi_message_rcv_it.o: ../Src/008spi_message_rcv_it.c \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h:
--- stm32f4xx_drivers/Debug/Src/subdir.mk
+++ stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -4,16 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/007spi_cmd_handling.c 
+../Src/008spi_message_rcv_it.c 
 
 OBJS += \
-./Src/007spi_cmd_handling.o 
+./Src/008spi_message_rcv_it.o 
 
 C_DEPS += \
-./Src/007spi_cmd_handling.d 
+./Src/008spi_message_rcv_it.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/007spi_cmd_handling.o: ../Src/007spi_cmd_handling.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -I"C:/Users/thaithinhtran/Downloads/Github/MCU1/stm32f4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/007spi_cmd_handling.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/008spi_message_rcv_it.o: ../Src/008spi_message_rcv_it.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -I"C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/008spi_message_rcv_it.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
