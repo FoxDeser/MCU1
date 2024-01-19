ff771be57e54b8d869b81cb56e5a74cc0951d5c8
Your Name <you@example.com>
2024-01-19 22:04:04 +0700

    Add other setting

---
+++ stm32f4xx_drivers/Debug/Src/012i2c_slave_tx_string.d
@@ -0,0 +1,13 @@
+Src/012i2c_slave_tx_string.o: ../Src/012i2c_slave_tx_string.c \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+
+C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h:
--- stm32f4xx_drivers/Debug/Src/subdir.mk
+++ stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -4,16 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/008spi_message_rcv_it.c 
+../Src/012i2c_slave_tx_string.c 
 
 OBJS += \
-./Src/008spi_message_rcv_it.o 
+./Src/012i2c_slave_tx_string.o 
 
 C_DEPS += \
-./Src/008spi_message_rcv_it.d 
+./Src/012i2c_slave_tx_string.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/008spi_message_rcv_it.o: ../Src/008spi_message_rcv_it.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -I"C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/008spi_message_rcv_it.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/012i2c_slave_tx_string.o: ../Src/012i2c_slave_tx_string.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -I"C:/Users/thaithinhtran/Downloads/Documents_MCU1/MCU1/stm32f4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/012i2c_slave_tx_string.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
