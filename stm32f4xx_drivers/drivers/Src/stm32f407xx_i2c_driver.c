0a71181024ba617e21bebc1bda2732133fcebd5f
Your Name <you@example.com>
2024-01-26 21:41:33 +0700

    Add the function for calculate baudrate

--- stm32f4xx_drivers/drivers/Src/stm32f407xx_i2c_driver.c
+++ stm32f4xx_drivers/drivers/Src/stm32f407xx_i2c_driver.c
@@ -63,56 +63,6 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
 		}
 }
 
-/*
- * Helper function
- */
-uint32_t RCC_GetPLLOutputClock()
-{
-	//Needs to be fixed
-	return 0;
-}
-uint32_t RCC_GetPCLK1Value(void)
-{
-	uint32_t pclk1,SystemClk;
-	uint8_t	clksrc,temp,AHBp,APBp;
-	uint16_t AHB_PreScaler[8] = {2,4,8,16,64,128,256,512};
-	uint16_t APB_PreScaler[4] = {2,4,8,16};
-
-	clksrc = (RCC->RCC_CFGR >>2) & 0x03;
-	if(clksrc == 0)
-	{
-		SystemClk = 16000000;
-	}else if (clksrc == 1)
-	{
-		SystemClk = 8000000;
-	}else if (clksrc == 2)
-	{
-		SystemClk = RCC_GetPLLOutputClock();
-	}
-
-	//For AHB
-	temp = (RCC->RCC_CFGR >>4) & 0xF;
-	if(temp < 8)
-	{
-		AHBp = 1;
-	}else
-	{
-		AHBp = AHB_PreScaler[temp-8];
-	}
-
-	//For APB
-	temp = (RCC->RCC_CFGR >>10) & 0x7;
-	if(temp < 4)
-	{
-		APBp = 1;
-	}else
-	{
-		APBp = APB_PreScaler[temp-4];
-	}
-
-	pclk1 = (SystemClk/AHBp)/APBp;
-	return pclk1;
-}
 /*********************************************************************
  * @fn      		  - I2C_Init
  *
